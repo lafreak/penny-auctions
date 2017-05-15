a42eb00add738654b7662b596940d956bd7a123d
Damian Kalinowski <damiikalii@gmail.com>
2017-05-16 01:23:38 +0200

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dp6r8zc2f'
+  config.api_key = '774583318999224'
+  config.api_secret = 'uKsQPoBx6uwJ-uM9a_RPUVDe2dc'
+  config.cdn_subdomain = true
+end
